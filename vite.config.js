3b53b2930143f30d5086846eca981cb0020c2a6a
Work-Simplified <gammongt@outlook.com>
2025-03-28 18:53:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 10000,  // This ensures Render can find the port to bind to
+    host: '0.0.0.0',  // This allows connections from external IPs (Render's server)
+  }
 })
