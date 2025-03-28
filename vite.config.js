41557644d996b68f538d2b3bf9a649066708984e
Work-Simplified <gammongt@outlook.com>
2025-03-28 18:55:46 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000,  // This ensures Render can find the port to bind to
-    host: '0.0.0.0',  // This allows connections from external IPs (Render's server)
+    port: 10000,  // Ensures Render can access the app
+    host: '0.0.0.0',  // Allow connections from external IPs (Render's server)
+    allowedHosts: ['residential-underwriting-tool.onrender.com']  // Add Render host here
   }
 })
